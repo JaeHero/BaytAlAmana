0e8ea996bd96e8aeae8f213fc1a463005d03d0a6
JaeHero <jd.hero@live.com>
2024-10-23 11:59:24 -0400

    Portfolio: Added My Investments

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { Investment } from '../models/investment';
+import { Router } from '@angular/router';
+import { InvestmentService } from '../services/investment.service';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule, TagModule, ButtonModule, ImageModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+  styleUrl: './portfolio.component.css',
 })
 export class PortfolioComponent {
+  investment: Investment[] = [];
+  responsiveOptions: any[] | undefined;
 
+  ngOnInit() {
+    //  this.investmentService.getInvestments().then((products) => {
+    //    this.products = products;
+    //  });
+    for (let index = 0; index < 6; index++) {
+      this.investment.push(this.investmentService.getInvestments());
+    }
+    console.log(this.investment);
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  constructor(
+    private router: Router,
+    private investmentService: InvestmentService
+  ) {}
+  goToDetails(investment: Investment) {
+    // Assuming investment.id is the unique identifier for the investment
+    this.router.navigate(['/investment-details', investment.id]);
+  }
 }

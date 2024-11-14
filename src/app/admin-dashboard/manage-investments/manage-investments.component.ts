d55e1dad3a7d8991120a91749dd11d6acd63527f
Your Name <you@example.com>
2024-11-14 14:48:00 -0500

    CRUD operations on Investments

--- src/app/admin-dashboard/manage-investments/manage-investments.component.ts
+++ src/app/admin-dashboard/manage-investments/manage-investments.component.ts
@@ -8,6 +8,10 @@ import { Router, RouterLink } from '@angular/router';
 import { FormsModule, FormGroup } from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { DropdownModule } from 'primeng/dropdown';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { Investment } from '../../models/investment';
+import { InvestmentService } from '../../services/investment.service';
 
 interface Status {
   status: string;
@@ -26,14 +30,25 @@ interface Status {
     FormsModule,
     InputNumberModule,
     DropdownModule,
+    CarouselModule,
+    TagModule,
   ],
   templateUrl: './manage-investments.component.html',
   styleUrl: './manage-investments.component.css',
 })
 export class ManageInvestmentsComponent implements OnInit {
   statuses: Status[] | undefined = [];
+  investment: Investment[] = [];
+
+  responsiveOptions: any[] | undefined;
 
   selectedStatus: Status | undefined;
+  showForm = false;
+
+  constructor(
+    private investmentService: InvestmentService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.statuses = [
@@ -41,10 +56,36 @@ export class ManageInvestmentsComponent implements OnInit {
       { status: 'In Progress' },
       { status: 'Closed' },
     ];
+    for (let index = 0; index < 6; index++) {
+      this.investment.push(this.investmentService.getInvestments());
+    }
+    console.log(this.investment);
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
   }
 
   createAccount() {}
   date: Date | undefined;
-  value1: number = 142000;
-  value2: number = 36600;
+  value1: number = 0;
+  value2: number = 0;
+
+  goToDetails(investment: Investment) {
+    // Assuming investment.id is the unique identifier for the investment
+    this.router.navigate(['/investment-details', investment.id]);
+  }
 }
